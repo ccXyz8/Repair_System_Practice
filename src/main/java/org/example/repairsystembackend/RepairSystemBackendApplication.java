d316a06fbcf4b98e5503aefb32c0ac6de0fe1607
ccXyz8 <1138456555@qq.com>
2025-06-19 17:15:56 +0800

    初始化

---
+++ src/main/java/org/example/repairsystembackend/RepairSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.repairsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/repairsystembackend/user/cbh.java
@@ -0,0 +1,4 @@
+package org.example.repairsystembackend.user;
+
+public class cbh {
+}
