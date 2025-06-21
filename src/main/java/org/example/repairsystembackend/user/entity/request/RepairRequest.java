ac1dbde1ccbdc3829b01c785b8bdfafe1dacdd60
ccXyz8 <1138456555@qq.com>
2025-06-21 16:26:48 +0800

    后端开发完成，并没有进行测试

--- src/main/java/org/example/repairsystembackend/user/entity/request/RepairRequest.java
+++ src/main/java/org/example/repairsystembackend/user/entity/request/RepairRequest.java
@@ -1,6 +1,5 @@
 package org.example.repairsystembackend.user.entity.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
